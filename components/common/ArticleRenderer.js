ca29b14e68283bdb0ab578291bf1f8857c788aef
winterlood <king199777@gmail.com>
2023-06-14 21:42:24 +0900

    ✨ /about 페이지 추가
    * PostBody 컴포넌트 공통 모듈로 변경 -> ArticleRenderer

--- components/common/ArticleRenderer.js
+++ components/common/ArticleRenderer.js
@@ -1,6 +1,6 @@
 import { NotionRenderer } from "react-notion-x";
 import { Code } from "react-notion-x/build/third-party/code";
-export default function PostBody({ pageBlock }) {
+export default function ArticleRenderer({ pageBlock }) {
   return (
     <NotionRenderer
       recordMap={pageBlock}
